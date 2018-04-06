c5775b5db23919da239cd559b78b7c87d2ba7180
Scott C. Livingston <scott@rerobots.net>
2018-04-06 13:58:18 -0700

    bootstrap script for case of single Linux host

---
+++ bootstrapping/linux-single.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -e
+# bootstrap a Linux workstation into a single-host workspace that uses
+# LXD as provider.
+#
+#
+# SCL <scott@rerobots.net>
+# Copyright (C) 2018 rerobots, Inc.
+set -e
+
+sudo apt-get -y install python3-virtualenv
+sudo apt -y install -t xenial-backports lxd lxd-client
+
+
+if ! grep \^rra /etc/passwd; then
+    sudo useradd -U -s /bin/sh -G lxd,dialout,plugdev -m rra
+fi
+sudo usermod -L rra
+
+export HOME=/home/rra
+cd $HOME
+sudo -u rra python3 -m virtualenv -p python3 PY3
+sudo -u rra bash -c "source ${HOME}/PY3/bin/activate && pip install -U pip && pip install requests pyjwt"
+sudo -u rra mkdir -p /home/rra/.ssh
+sudo -u rra sh -c "echo 'StrictHostKeyChecking yes' > /home/rra/.ssh/config"
