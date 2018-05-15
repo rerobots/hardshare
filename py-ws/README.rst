fe1093026893da30d0bd102027568b32977d8852
Scott C. Livingston <scott@rerobots.net>
2018-05-15 15:47:25 -0700

    init client implemented in Python

---
+++ py-ws/README.rst
@@ -0,0 +1,12 @@
+hardshare
+=========
+
+Introduction
+------------
+
+**hardshare** is the client part of a system for sharing your robot hardware
+through the rerobots infrastructure. It is in the early stages of development.
+Until the first released version, the protocol can change without warning.
+
+This Python package provides the CLI and WebSocket-based daemon for the client
+side. The official repository is at https://github.com/rerobots/hardshare
---
+++ py-ws/hardshare/cli.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# Copyright (C) 2018 rerobots, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Command-line interface
+"""
+import argparse
+import sys
+import time
+
+
+def main(argv=None):
+    aparser = argparse.ArgumentParser(description=('Command-line interface'
+                                                   ' for the hardshare client'))
+    aparser.add_argument('-V', '--version', action='store_true', default=False,
+                         help='print version of hardshare (this) package.',
+                         dest='print_version')
+    argv_parsed = aparser.parse_args(argv)
+
+    if argv_parsed.print_version:
+        from . import __version__ as hardshare_pkg_version
+        print(hardshare_pkg_version)
+        return 0
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
