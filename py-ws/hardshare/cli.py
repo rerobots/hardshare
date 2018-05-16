83320954dbea534482f04eb2e748d2126912ef8b
Scott C. Livingston <scott@rerobots.net>
2018-05-16 16:57:34 -0700

    add subcommands

--- py-ws/hardshare/cli.py
+++ py-ws/hardshare/cli.py
@@ -19,20 +19,52 @@ import sys
 
 
 def main(argv=None):
-    aparser = argparse.ArgumentParser(description=('Command-line interface'
-                                                   ' for the hardshare client'))
-    aparser.add_argument('-V', '--version', action='store_true', default=False,
-                         help='print version of hardshare (this) package.',
-                         dest='print_version')
-    argv_parsed = aparser.parse_args(argv)
+    if argv is None:
+        argv = sys.argv[1:]
+    argparser = argparse.ArgumentParser(description=('Command-line interface'
+                                                     ' for the hardshare client'))
+    argparser.add_argument('-V', '--version', action='store_true', default=False,
+                           help='print version of hardshare (this) package.',
+                           dest='print_version')
+    argparser.add_argument('-s', '--server-name', default='hs.rerobots.net',
+                           help='name or IP address of hardshare server',
+                           dest='server_name')
+    argparser.add_argument('-k', '--insecure', action='store_true', default=False,
+                           help=('communications with hardshare servers always use TLS.'
+                                 ' this switch causes certificates to not be verified.'),
+                           dest='ignore_certs')
 
-    if argv_parsed.print_version:
+    subparsers = argparser.add_subparsers(dest='command')
+
+    subparsers.add_parser('version', help='print version number and exit.')
+    subparsers.add_parser('help', help='print this help message and exit')
+
+    register_commanddesc = 'register new workspace deployment'
+    register_parser = subparsers.add_parser('register',
+                                            description=register_commanddesc,
+                                            help=register_commanddesc)
+
+    advertise_commanddesc = 'advertise availability, accept new instances'
+    advertise_parser = subparsers.add_parser('ad',
+                                             description=advertise_commanddesc,
+                                             help=advertise_commanddesc)
+    advertise_parser.add_argument('-d', '--daemon', action='store_true', default=False,
+                                  help='detach from invoking terminal (i.e., run as daemon)',
+                                  dest='become_daemon')
+
+    argv_parsed = argparser.parse_args(argv)
+
+    if argv_parsed.print_version or argv_parsed.command == 'version':
         from . import __version__ as hardshare_pkg_version
         print(hardshare_pkg_version)
+        return 0
+
+    if argv_parsed.command is None or argv_parsed.command == 'help':
+        argparser.print_help()
         return 0
 
     return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv[1:]))
