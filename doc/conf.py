2a5cb6e7bb754ce472fb99b22a8dca857bfe5f56
Scott C. Livingston <scott@rerobots.net>
2018-05-28 15:12:50 -0700

    DOC: init

---
+++ doc/conf.py
@@ -0,0 +1,20 @@
+project = 'hardshare'
+copyright = '2018 rerobots, Inc.'
+author = 'rerobots, Inc.'
+
+version = ''
+release = ''
+
+extensions = [
+    'sphinx.ext.mathjax',
+]
+
+source_suffx = '.rst'
+exclude_patterns = ['_build']
+master_doc = 'index'
+
+language = None
+pygments_style = 'sphinx'
+html_theme = 'alabaster'
+html_static_path = ['_static']
+htmlhelp_basename = 'hardsharedoc'
---
+++ doc/index.rst
@@ -0,0 +1,15 @@
+hardshare
+=========
+
+Introduction
+------------
+
+**hardshare** is the client part of a system for sharing your robot hardware
+through the rerobots infrastructure. It is in the early stages of development.
+Until the first released version, the API can change without warning.
+
+.. toctree::
+   :numbered:
+   :maxdepth: 1
+
+   quickstart
