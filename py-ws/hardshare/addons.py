c640ef5d31ab9dca06ba9cb92d189fd5aeb136c2
Scott C. Livingston <scott@rerobots.net>
2020-06-19 16:59:36 -0700

    addon: retry WebSocket connection if lost

--- py-ws/hardshare/addons.py
+++ py-ws/hardshare/addons.py
@@ -10,6 +10,7 @@ Copyright (C) 2020 rerobots, Inc.
 import asyncio
 import base64
 from io import BytesIO
+import logging
 import sys
 import time
 
@@ -26,6 +27,9 @@ import requests
 #   PIL
 
 
+logger = logging.getLogger(__name__)
+
+
 async def camera_upload(hscamera_id, dev, tok, rotate, width, height):
     import cv2
     from PIL import Image
@@ -33,41 +37,53 @@ async def camera_upload(hscamera_id, dev, tok, rotate, width, height):
     ENCODING_PREFIX = 'data:image/jpeg;base64,'
 
     headers = {'Authorization': 'Bearer {}'.format(tok)}
-    session = aiohttp.ClientSession(headers=headers)
     uri = 'https://api.rerobots.net/hardshare/cam/{}/upload'.format(hscamera_id)
-    async with session.ws_connect(uri, timeout=30.0, autoping=True) as ws:
-        cam = cv2.VideoCapture(dev)
-        adjusted = False if width is not None else True
+    active = True
+    while active:
+        session = aiohttp.ClientSession(headers=headers)
+        try:
+            async with session.ws_connect(uri, timeout=30.0, autoping=True) as ws:
+                cam = cv2.VideoCapture(dev)
+                adjusted = False if width is not None else True
 
-        timeout = 3
-        switched = False
-        st = time.time()
-        while True:
-            if not cam.isOpened():
-                if not switched and (time.time() - st >= timeout):
-                    switched = True
-                    cam = cv2.VideoCapture('/dev/video{}'.format(dev))
-                time.sleep(1)
-                continue
-            if not adjusted and width and cam.get(cv2.CAP_PROP_FRAME_HEIGHT) != height:
-                if cam.set(cv2.CAP_PROP_FRAME_WIDTH, width) or cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height):
-                    adjusted = True
-            ret, frame = cam.read()
-            img = Image.fromarray(frame[:,:,[2,1,0]], mode='RGB')
-            if rotate == 270:
-                img = img.transpose(Image.ROTATE_270)
-            elif rotate == 90:
-                img = img.transpose(Image.ROTATE_90)
-            elif rotate == 180:
-                img = img.transpose(Image.ROTATE_180)
-            elif rotate == 0:
-                pass
-            elif rotate is not None:
-                pass  # TODO: general case
-            buf = BytesIO()
-            img.save(buf, 'JPEG')
-            await ws.send_str(ENCODING_PREFIX + base64.b64encode(buf.getvalue()).decode('utf-8'))
-            time.sleep(0.1)
+                timeout = 3
+                switched = False
+                st = time.time()
+                while True:
+                    if not cam.isOpened():
+                        if not switched and (time.time() - st >= timeout):
+                            switched = True
+                            cam = cv2.VideoCapture('/dev/video{}'.format(dev))
+                        time.sleep(1)
+                        continue
+                    if not adjusted and width and cam.get(cv2.CAP_PROP_FRAME_HEIGHT) != height:
+                        if cam.set(cv2.CAP_PROP_FRAME_WIDTH, width) or cam.set(cv2.CAP_PROP_FRAME_HEIGHT, height):
+                            adjusted = True
+                    ret, frame = cam.read()
+                    img = Image.fromarray(frame[:,:,[2,1,0]], mode='RGB')
+                    if rotate == 270:
+                        img = img.transpose(Image.ROTATE_270)
+                    elif rotate == 90:
+                        img = img.transpose(Image.ROTATE_90)
+                    elif rotate == 180:
+                        img = img.transpose(Image.ROTATE_180)
+                    elif rotate == 0:
+                        pass
+                    elif rotate is not None:
+                        pass  # TODO: general case
+                    buf = BytesIO()
+                    img.save(buf, 'JPEG')
+                    await ws.send_str(ENCODING_PREFIX + base64.b64encode(buf.getvalue()).decode('utf-8'))
+                    time.sleep(0.1)
+
+        except asyncio.CancelledError:
+            active = False
+
+        except Exception as err:
+            logger.error('caught {}: {}'.format(type(err), err))
+
+        finally:
+                await session.close()
 
 
 def camera_main(wdeployments, tok, dev, rotate=None, width=None, height=None):
