232e09b1251eee1ef16c70fdbfb9b2cf012fbb69
Scott C. Livingston <scott@rerobots.net>
2020-10-13 15:27:22 -0700

    TEST: different spellings of CLI help, version
    following pattern of CLI tests in
    https://github.com/rerobots/py.git

--- py-ws/tests/test_cli.py
+++ py-ws/tests/test_cli.py
@@ -22,6 +22,31 @@ def test_version():
     sys.stdout = original_stdout
     assert hardshare.__version__ == res
 
+
+def test_alternative_version_spellings():
+    original_stdout = sys.stdout
+
+    # `version`
+    sys.stdout = StringIO()
+    cli.main(['version'])
+    res_version = sys.stdout.getvalue().strip()
+
+    # `-V`
+    sys.stdout = StringIO()
+    cli.main(['-V'])
+    res_dashV = sys.stdout.getvalue().strip()
+
+    # `--version`
+    sys.stdout = StringIO()
+    cli.main(['--version'])
+    res_dashdashversion = sys.stdout.getvalue().strip()
+
+    sys.stdout = original_stdout
+
+    assert res_version == res_dashV
+    assert res_version == res_dashdashversion
+
+
 def test_help():
     original_stdout = sys.stdout
     sys.stdout = StringIO()
@@ -29,6 +54,30 @@ def test_help():
     res = sys.stdout.getvalue().strip()
     sys.stdout = original_stdout
     assert 'Command-line interface for the hardshare client' in res
+
+
+def test_alternative_help_spellings():
+    original_stdout = sys.stdout
+
+    # `help`
+    sys.stdout = StringIO()
+    cli.main(['help'])
+    res_help = sys.stdout.getvalue().strip()
+
+    # `-h`
+    sys.stdout = StringIO()
+    cli.main(['-h'])
+    res_dashh = sys.stdout.getvalue().strip()
+
+    # `--help`
+    sys.stdout = StringIO()
+    cli.main(['--help'])
+    res_dashdashhelp = sys.stdout.getvalue().strip()
+
+    sys.stdout = original_stdout
+
+    assert res_help == res_dashh
+    assert res_help == res_dashdashhelp
 
 
 if __name__ == '__main__':
