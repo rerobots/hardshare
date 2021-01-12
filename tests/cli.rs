592b21c7d8da12600b3cbd1a16a03cb6c8354347
Scott C. Livingston <scott@rerobots.net>
2021-01-11 18:49:19 -0800

    TEST: `hardshare config` requires arguments

--- tests/cli.rs
+++ tests/cli.rs
@@ -12,3 +12,11 @@ fn prints_version() {
     let assert = cmd.arg("-V").assert();
     assert.stdout(format!("{}\n", env!("CARGO_PKG_VERSION"))).success();
 }
+
+
+#[test]
+fn config_requires_arg() {
+    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+    let assert = cmd.arg("config").assert();
+    assert.failure().code(1);
+}
