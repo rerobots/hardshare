305e46ec416baa0548d5457b3e1d04e20563bc39
Scott C. Livingston <scott@rerobots.net>
2024-06-14 19:04:57 -0700

    DOC: move hardshare docs to be part of rerobots docs
    It is too confusing to host hardshare docs separately, so these
    will instead be part of docs repository, currently at
    https://github.com/rerobots/docs.git

--- doc/next.config.js
+++
@@ -1,7 +0,0 @@
-const withMarkdoc = require('@markdoc/next.js');
-
-module.exports = withMarkdoc({
-    schemaPath: 'src/markdoc',
-})({
-    pageExtensions: ['ts', 'tsx', 'md'],
-});
--- doc/src/components/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './CodeBlock';
-export * from './Heading';
-export * from './Image';
-export * from './Link';
-export * from './Vimeo';
