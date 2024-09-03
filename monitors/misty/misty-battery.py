0b5f49339d3c1df0487783eaa154279948224308
Scott C. Livingston <scott@rerobots.net>
2024-09-03 14:52:29 -0700

    monitor: allow retries in Misty battery checker

--- monitors/misty/misty-battery.py
+++ monitors/misty/misty-battery.py
@@ -11,28 +11,35 @@ import requests
 
 
 if __name__ == '__main__':
-    double_check_interval = 60*5  # 5 minutes
     misty_ip_addr = os.environ.get('REROBOTS_MISTY_IP') or sys.argv[1]
     battery_thr = 0.5
     battery_msg_title = 'ALERT: Misty battery level is low!'
+    retry_allowance = 1  # At least 0
 
-    err_message = None
-    alt_title = None
-    try:
-        res = requests.get('http://' + misty_ip_addr + '/api/battery', timeout=5)
-        if not res.ok:
-            err_message = 'Received not OK response: {}'.format(res)
-        else:
-            payload = res.json()
-            if payload['result']['chargePercent'] < battery_thr:
-                alt_title = battery_msg_title
-                err_message = 'Misty battery level is {} (below threshold of {})'.format(payload['result']['chargePercent'], battery_thr)
-    except Exception as err:
-        err_message = 'Exception raised while trying to contact Misty: {}: {}'.format(type(err), err)
-
-    if err_message is not None:
-        print('failed to communicate with Misty at: {}'.format(datetime.now()))
+    while True:
+        err_message = None
+        alt_title = None
         try:
+            res = requests.get('http://' + misty_ip_addr + '/api/battery', timeout=5)
+            if not res.ok:
+                err_message = 'Received not OK response: {}'.format(res)
+            else:
+                payload = res.json()
+                if payload['result']['chargePercent'] < battery_thr:
+                    alt_title = battery_msg_title
+                    err_message = 'Misty battery level is {} (below threshold of {})'.format(payload['result']['chargePercent'], battery_thr)
+        except Exception as err:
+            err_message = 'Exception raised while trying to contact Misty: {}: {}'.format(type(err), err)
+
+        if err_message is None:
+            break
+
+        retry_allowance -= 1
+        if retry_allowance >= 0:
+            print(f'Detected error, but there are {retry_allowance+1} retries remaining. Sleeping and checking again...')
+            time.sleep(1)
+        else:
+            print('failed to communicate with Misty at: {}'.format(datetime.now()))
             if alt_title:
                 title = alt_title
             else:
@@ -40,5 +47,3 @@ if __name__ == '__main__':
             print(title)
             print(err_message)
             sys.exit(1)
-        except Exception as err:
-            print('error ({}: {}) while sending alert; sleeping and trying later'.format(type(err), err))
