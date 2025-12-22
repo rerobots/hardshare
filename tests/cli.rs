6e77b92f8a3f6ff5bfa9cdbd92f245f873b47c08
Scott C. Livingston <scott@rerobots.net>
2025-12-22 12:08:53 -0800

    TEST: refactor to use fewer unwrap()

--- tests/cli.rs
+++ tests/cli.rs
@@ -15,122 +15,118 @@
 use assert_cmd::Command;
 use tempfile::{tempdir, NamedTempFile};
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn prints_version() {
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+fn prints_version() -> TestResult {
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("-V").assert();
     assert
         .stdout(format!("{}\n", env!("CARGO_PKG_VERSION")))
         .success();
+    Ok(())
 }
 
 #[test]
-fn prints_help() {
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+fn prints_help() -> TestResult {
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(String::from_utf8(output.stdout).unwrap());
+    insta::assert_snapshot!(String::from_utf8(output.stdout)?);
 
     // Alternative style: -h
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("-h").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout).unwrap());
+    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout)?);
 
     // Alternative style: --help
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("--help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout).unwrap());
+    insta::assert_snapshot!("prints_help", String::from_utf8(output.stdout)?);
+
+    Ok(())
 }
 
 #[test]
-fn prints_help_config() {
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+fn prints_help_config() -> TestResult {
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("help").arg("config").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(String::from_utf8(output.stdout).unwrap());
+    insta::assert_snapshot!(String::from_utf8(output.stdout)?);
 
     // Alternative style: -h
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("config").arg("-h").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_config",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_config", String::from_utf8(output.stdout)?);
 
     // Alternative style: --help
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("config").arg("--help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_config",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_config", String::from_utf8(output.stdout)?);
+
+    Ok(())
 }
 
 #[test]
-fn prints_help_register() {
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+fn prints_help_register() -> TestResult {
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("help").arg("register").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_register",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_register", String::from_utf8(output.stdout)?);
 
     // Alternative style: -h
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("register").arg("-h").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_register",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_register", String::from_utf8(output.stdout)?);
 
     // Alternative style: --help
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("register").arg("--help").assert();
     let output = assert.get_output().clone();
     assert.success();
-    insta::assert_snapshot!(
-        "prints_help_register",
-        String::from_utf8(output.stdout).unwrap()
-    );
+    insta::assert_snapshot!("prints_help_register", String::from_utf8(output.stdout)?);
+
+    Ok(())
 }
 
 #[test]
-fn config_requires_arg() {
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+fn config_requires_arg() -> TestResult {
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd.arg("config").assert();
     assert.failure().code(1);
+    Ok(())
 }
 
 #[test]
-fn add_token_does_not_exist() {
-    let ntf = NamedTempFile::new().unwrap();
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+fn add_token_does_not_exist() -> TestResult {
+    let ntf = NamedTempFile::new()?;
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd
         .arg("config")
         .arg("--add-key")
         .arg(ntf.path().join("notexist"))
         .assert();
     assert.failure().code(1);
+    Ok(())
 }
 
 #[test]
-fn list_config_does_not_exist() {
-    let tmphome = tempdir().unwrap();
-    let mut cmd = Command::cargo_bin("hardshare").unwrap();
+fn list_config_does_not_exist() -> TestResult {
+    let tmphome = tempdir()?;
+    let mut cmd = Command::cargo_bin("hardshare")?;
     let assert = cmd
         .env("HOME", tmphome.path())
         .arg("config")
@@ -138,4 +134,5 @@ fn list_config_does_not_exist() {
         .arg("-l")
         .assert();
     assert.failure().code(1);
+    Ok(())
 }
