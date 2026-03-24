4f2f696a879742b5d4b3bdf35cea0e086380137a
Scott C. Livingston <scott@rerobots.net>
2026-03-24 16:34:20 -0700

    use extra_scripts to hot-swap PlatformIO upload protocol

---
+++ integrations/platformio/rerobots_pio.py
@@ -0,0 +1,16 @@
+# Copyright (C) 2026 rerobots, Inc.
+
+Import('env')
+
+import os
+
+
+if 'REROBOTS_PLATFORMIO' in os.environ:
+    env.Replace(UPLOAD_PROTOCOL='custom')
+    env.Replace(UPLOADCMD='platformio-proxy c ' + os.environ['REROBOTS_PLATFORMIO'] + ' $PROJECT_CONFIG $PROGPATH')
+    env.Replace(UPLOAD_PORT='/dev/null')
+
+    def rrserial_cb(*args, **kwargs):
+        env.Execute('pio device monitor -p socket://$REROBOTS_SERIAL')
+
+    env.AddCustomTarget("rrserial", None, rrserial_cb)
